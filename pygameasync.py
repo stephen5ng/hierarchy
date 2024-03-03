064ba0e52b39887727d463376e94cbd2224ac545
Stephen Ng <stephen5.ng@gmail.com>
2024-03-02 16:53:43 -0800

    Extract common async code.

---
+++ pygameasync.py
@@ -0,0 +1,70 @@
+from aiohttp import web
+import asyncio
+from datetime import datetime
+import json
+import pygame
+
+class Clock:
+    def __init__(self, time_func=pygame.time.get_ticks):
+        self.time_func = time_func
+        self.last_tick = time_func() or 0
+
+    async def tick(self, fps=0):
+        if 0 >= fps:
+            return
+
+        end_time = (1.0 / fps) * 1000
+        current = self.time_func()
+        time_diff = current - self.last_tick
+        delay = (end_time - time_diff) / 1000
+
+        self.last_tick = current
+        if delay < 0:
+            delay = 0
+
+        await asyncio.sleep(delay)
+
+class EventEngine:
+    def __init__(self):
+        self.listeners = {}
+
+    def on(self, event):
+        if event not in self.listeners:
+            self.listeners[event] = []
+
+        def wrapper(func, *args):
+            self.listeners[event].append(func)
+            return func
+
+        return wrapper
+
+    # this function is purposefully not async
+    # code calling this will do so in a "fire-and-forget" manner, and shouldn't be slowed down by needing to await a result
+    def trigger(self, event, *args, **kwargs):
+        asyncio.create_task(self.async_trigger(event, *args, **kwargs))
+
+    # whatever gets triggered is just added to the current asyncio event loop, which we then trust to run eventually
+    async def async_trigger(self, event, *args, **kwargs):
+        if event in self.listeners:
+            handlers = [func(*args, **kwargs) for func in self.listeners[event]]
+
+            # schedule all listeners to run
+            return await asyncio.gather(*handlers)
+
+
+
+class WebFrontend:
+    def __init__(self, port=8081):
+        self.port = port
+        self.runner = None
+        self.app = web.Application()
+
+    async def startup(self):
+        self.runner = web.AppRunner(self.app)
+        await self.runner.setup()
+        site = web.TCPSite(self.runner, "localhost", self.port)
+        await site.start()
+
+    async def shutdown(self):
+        if self.runner:
+            await self.runner.cleanup()
--- pygamegameasync.py
+++ pygamegameasync.py
@@ -7,54 +7,7 @@ from datetime import datetime
 import json
 import pygame
 
-
-class Clock:
-    def __init__(self, time_func=pygame.time.get_ticks):
-        self.time_func = time_func
-        self.last_tick = time_func() or 0
-
-    async def tick(self, fps=0):
-        if 0 >= fps:
-            return
-
-        end_time = (1.0 / fps) * 1000
-        current = self.time_func()
-        time_diff = current - self.last_tick
-        delay = (end_time - time_diff) / 1000
-
-        self.last_tick = current
-        if delay < 0:
-            delay = 0
-
-        await asyncio.sleep(delay)
-
-class EventEngine:
-    def __init__(self):
-        self.listeners = {}
-
-    def on(self, event):
-        if event not in self.listeners:
-            self.listeners[event] = []
-
-        def wrapper(func, *args):
-            self.listeners[event].append(func)
-            return func
-
-        return wrapper
-
-    # this function is purposefully not async
-    # code calling this will do so in a "fire-and-forget" manner, and shouldn't be slowed down by needing to await a result
-    def trigger(self, event, *args, **kwargs):
-        asyncio.create_task(self.async_trigger(event, *args, **kwargs))
-
-    # whatever gets triggered is just added to the current asyncio event loop, which we then trust to run eventually
-    async def async_trigger(self, event, *args, **kwargs):
-        if event in self.listeners:
-            handlers = [func(*args, **kwargs) for func in self.listeners[event]]
-
-            # schedule all listeners to run
-            return await asyncio.gather(*handlers)
-
+from pygameasync import Clock, EventEngine, WebFrontend
 
 events = EventEngine()
 
@@ -109,11 +62,9 @@ class Game:
             await player.update(window)
 
 
-class WebFrontend:
+class CubeWebFrontend(WebFrontend):
     def __init__(self, port=8081):
-        self.port = port
-        self.runner = None
-        self.app = web.Application()
+        super().__init__(port)
 
         self.app.add_routes(
             [
@@ -130,16 +81,6 @@ class WebFrontend:
                 await resp.send(data)
                 await asyncio.sleep(1)
         return resp
-
-    async def startup(self):
-        self.runner = web.AppRunner(self.app)
-        await self.runner.setup()
-        site = web.TCPSite(self.runner, "localhost", self.port)
-        await site.start()
-
-    async def shutdown(self):
-        if self.runner:
-            await self.runner.cleanup()
 
 
 async def main():
