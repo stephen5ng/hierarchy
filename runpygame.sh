052f00753f262eeb90d1a38f5b0c8a977170c79e
Stephen Ng <stephen5.ng@gmail.com>
2025-01-21 17:59:00 -0800

    use env variable

--- runpygame.sh
+++ runpygame.sh
@@ -3,17 +3,11 @@
 . cube_env/bin/activate
 
 trap "kill 0" EXIT
-if ! nc -zv localhost 1883 > /dev/null 2>&1; then
+mqtt_server=${MQTT_SERVER:-localhost}
+if ! nc -zv $mqtt_server 1883 > /dev/null 2>&1; then
   /opt/homebrew/opt/mosquitto/sbin/mosquitto -c /opt/homebrew/etc/mosquitto/mosquitto.conf &
 fi
 
-has_esp_32() {
-  local pattern="$2"
-  if [[ $(find "/dev" -maxdepth 1 \( -name "ttyUSB*" -o -name "cu.usb*" \) -print -quit) ]]; then
-    return 0  # True if a matching file is found
-  fi
-  return 1  # False if no matching files are found
-}
 
-python -X dev -X tracemalloc=5 ./main.py
-# python ./main.py
+#python -X dev -X tracemalloc=5 ./main.py
+python ./main.py
